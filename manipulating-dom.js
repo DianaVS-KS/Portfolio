894e91430b2bab926544649c62f71df4b5c92493
DianaVS-KS <diana.villalvazo@theksquaregroup.com>
2021-04-14 18:22:14 -0500

    fix button view more

--- manipulating-dom.js
+++ manipulating-dom.js
@@ -1,11 +1,14 @@
 const showMore = function () {
     const elementsHid = document.getElementsByClassName('hid');
+    const button = document.getElementById('view');
     for(let el of elementsHid){
         if(el.style.display === 'none'){
             el.style.display = 'inline';
+            button.innerText = 'View Less';
         }
         else {
             el.style.display = 'none';
+            button.innerText = 'View More';
         }
     }
 }
